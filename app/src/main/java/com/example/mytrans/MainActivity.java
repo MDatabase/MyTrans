f1d3e266c1dd61c637de9b6a2fe46c565ff0910a
MDatabase <eoeksgksdnfl@naver.com>
2019-10-02 17:37:33 +0900

    making toggle button

--- app/src/main/java/com/example/mytrans/MainActivity.java
+++ app/src/main/java/com/example/mytrans/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Spinner tT,tbT;
     ArrayList<String> langList;
     ArrayAdapter<String> arrayAdapter;
+    Button OnOffBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +32,31 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
                 android.R.layout.simple_spinner_dropdown_item,langList);
 
-        tT=(Spinner)findViewById(R.id.tTSpin);
+        tT=findViewById(R.id.tTSpin);
         tT.setAdapter(arrayAdapter);
         tT.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
-
+                Toast.makeText(getApplicationContext(),langList.get(i)+"가 선택되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
+        tbT=findViewById(R.id.tbTSpin);
+        tbT.setAdapter(arrayAdapter);
+        tbT.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
+                Toast.makeText(getApplicationContext(),langList.get(i)+"가 선택되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
 
 
 
