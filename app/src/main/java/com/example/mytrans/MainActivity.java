700060281c111d0675da2d0e4dc8cdea11a721d0
MDatabase <eoeksgksdnfl@naver.com>
2019-10-02 16:54:50 +0900

    making spinner

--- app/src/main/java/com/example/mytrans/MainActivity.java
+++ app/src/main/java/com/example/mytrans/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.mytrans;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner tT,tbT;
+    ArrayList<String> langList;
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        langList=new ArrayList<>();
+        langList.add("한국어");
+        langList.add("영어");
+        langList.add("일본어");
+        langList.add("중국어");
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item,langList);
+
+        tT=(Spinner)findViewById(R.id.tTSpin);
+        tT.setAdapter(arrayAdapter);
+        tT.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
+
+            }
+        });
+
+
+
     }
 }
