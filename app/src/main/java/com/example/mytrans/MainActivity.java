063cabeccfa5f78fd8c6281ae15c8557877aac0f
MDatabase <eoeksgksdnfl@naver.com>
2019-10-07 14:32:55 +0900

    add floating button

--- app/src/main/java/com/example/mytrans/MainActivity.java
+++ app/src/main/java/com/example/mytrans/MainActivity.java
@@ -2,26 +2,54 @@ package com.example.mytrans;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int DRAW_OVER_OTHER_APP_PERMISSION = 123;
     private Spinner tT,tbT;
     ArrayList<String> langList;
     ArrayAdapter<String> arrayAdapter;
     Button OnOffBtn;
+    Button fBtn;
+    TextView fTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        askForSystemOverlayPermission(); // 퍼미션을 묻는 함수
+
+        fBtn=findViewById(R.id.fbutton);
+        fTxt=findViewById(R.id.ftextView);
+        int badge_count = getIntent().getIntExtra("badge_count", 0);
+
+        fTxt.setText(badge_count + " messages received previously");
+
+
+        fBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(MainActivity.this)) {
+                    startService(new Intent(MainActivity.this, FloatingWidgetService.class));
+                } else {
+                    errorToast();
+                }
+            }
+        });
+
 
         langList=new ArrayList<>();
         langList.add("한국어");
@@ -61,4 +89,33 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void askForSystemOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            //If the draw over permission is not available to open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION);
+        }
+    }
+    protected void onPause() {
+        super.onPause();
+
+
+        // To prevent starting the service if the required permission is NOT granted.
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                //Permission is not available. Display error text.
+                Toast.makeText(this, "Draw over other app permission not available. Can't start the application without the permission.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }else {
+        //super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    private void errorToast() {
+        Toast.makeText(this, "Draw over other app permission not available. Can't start the application without the permission.", Toast.LENGTH_LONG).show();
+    }
 }
+
